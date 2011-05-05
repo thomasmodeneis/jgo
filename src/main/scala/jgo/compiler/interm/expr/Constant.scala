9343d4c4aabafab3a67652ba1a0fd57686af2d7e
Harrison Klaperman <Harrison Klaperman@localhost>
2011-05-05 15:58:25 -0400

    Even more Constant

--- src/main/scala/jgo/compiler/interm/expr/Constant.scala
+++ src/main/scala/jgo/compiler/interm/expr/Constant.scala
@@ -13,52 +13,45 @@ sealed abstract class Constant extends Expr {
   
 }
 
+private sealed abstract class NumericConst {
+  val value: BigDecimal
+}
+
 private case class UntypedNumericConst(value: BigDecimal) extends Constant {
   def eval =
     throw new UnsupportedOperationException("Impl error: attempting to call eval on an UntypedConst")
   
+  def isBounded(low: BigDecimal, high: BigDecimal) =
+    (low <= value) && (value <= high)
+    
+  def isInt =
+    value % 1 == 0
+    
   val typeOf = new UntypedConstType {
-    private def bounded(low: BigDecimal, high: BigDecimal) =
-      (low <= value) && (value <= high)
-    
-    private def 
-    
     def canFitIn(t: BuiltinType) = cond(t) {
       case Int8  => value.isValidByte
       case Int16 => value.isValidShort
       case Int32 => value.isValidInt
-      case Int64 => bounded(Long.MinValue, Long.MaxValue)
+      case Int64 => isInt && isBounded(Long.MinValue, Long.MaxValue)
       
-      case Uint8  => bounded(0, 255)
-      case Uint16 => bounded(0, (1 << 16) - 1)
-      case Uint32 => bounded(0, (1 << 32) - 1)
-      case Uint64 => bounded(0, (1 << 64) - 1)
+      case Uint8  => isInt && isBounded(0, 255)
+      case Uint16 => isInt && isBounded(0, (1 << 16) - 1)
+      case Uint32 => isInt && isBounded(0, (1 << 32) - 1)
+      case Uint64 => isInt && isBounded(0, (1 << 64) - 1)
       
       case Float32 => value.floatValue.isInfinite //Float.MinValue <= value && value <=  Float.MaxValue
       case Float64 => value.doubleValue.isInfinite //Double.MinValue <= value && value <= Double.MaxValue
       
-      case Complex64  => value.floatValue.isInfinite
-      case Complex128 => value.doubleValue.isInfinite
-      
+      case Complex64  => throw new UnsupportedOperationException("complex constants not yet supported") //value.floatValue.isInfinite
+      case Complex128 => throw new UnsupportedOperationException("complex constants not yet supported") //value.doubleValue.isInfinite
     }
-    
-    /*
-    def withType(newType: NumericType): M[Constant] = newType match {
-      
-    }
-    */
     
     val semantics = Primitive
   }
+    
+  /*
+  def withType(newType: NumericType): M[Constant] = newType match {
+    
+  }
+  */
 }
-
-private sealed abstract class TypedConst {
-  val typeOf: ConstableType
-}
-
-private case class IntegralConst(value: Long, typeOf: IntegralType) extends Constant {
-  def eval = IntConst(value, typeOf)
-  val semantics  = Primitive
-}
-
-private case class FloatingPtConst(value: Double
