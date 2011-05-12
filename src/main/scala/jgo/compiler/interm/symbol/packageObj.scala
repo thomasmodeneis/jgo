9453e5efd10ca1650e831ea727d8096a81b24fce
Harrison Klaperman <Harrison Klaperman@localhost>
2011-05-11 21:07:50 -0400

    Oops.  Forgot to remove that.

--- src/main/scala/jgo/compiler/interm/symbol/packageObj.scala
+++
@@ -1,9 +0,0 @@
-package jgo.compiler
-package interm
-
-import types._
-
-package object symbol {
-  implicit def name2symbol(nt: NamedType):  TypeSymbol = TypeSymbol(nt)
-  implicit def symbol2name(ts: TypeSymbol): NamedType  = ts.theType
-}
