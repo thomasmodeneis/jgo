5a04c1087a6d683fa92383deb7b2f82f22e6f8d6
Harrison Klaperman <Harrison Klaperman@localhost>
2011-06-02 19:31:18 -0400

    Set up project to use sxr.  In other news, scaladoc refuses to generate documentation, reporting 83 phantom compiler errors that the compiler itself does not.  As far as I can tell, all of these errors involve uses of the ill-named monad M.  Huh.

--- project/build/GoCompilerProject.scala
+++ project/build/GoCompilerProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 import reaktor.scct.ScctProject
 
-class GoCompilerProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject {
+class GoCompilerProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
   //val kiama = "com.googlecode" %% "kiama" % "[1.0.2,)" withSources()
   //val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "[1.8,)" withSources() withJavadoc()
   //val scalatest = "org.scalatest" % "scalatest" % "[1.3,)"
@@ -9,7 +9,15 @@ class GoCompilerProject(info: ProjectInfo) extends DefaultProject(info) with Scc
   val core = "com.github.scala-incubator.io" %% "core" % "[0.1.2,)"
   val file = "com.github.scala-incubator.io" %% "file" % "[0.1.2,)"
   
-  override def compileOptions = super.compileOptions ++ Seq(Unchecked, ExplainTypes) //++
+  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "[0.2.7,)")
+  
+  override def compileOptions =
+    super.compileOptions :::
+    Unchecked ::
+    CompileOption("-Xplugin-list") ::
+    CompileOption("-P:sxr:base-directory:" + mainScalaSourcePath.absolutePath) ::
+    Nil
+     //++
     //compileOptions("-Xlog-implicits") //-Yclosure-elim -Ydead-code -Yinline -optimise
   
   override def compileOrder = CompileOrder.ScalaThenJava
