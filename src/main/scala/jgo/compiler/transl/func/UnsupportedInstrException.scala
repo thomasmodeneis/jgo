c318b1e48ab48d7ba3533eca8ecfaa4c7d5417ce
Harrison Klaperman <Harrison Klaperman@localhost>
2011-06-09 16:13:06 -0400

    Beginning of new func translation module.

---
+++ src/main/scala/jgo/compiler/transl/func/UnsupportedInstrException.scala
@@ -0,0 +1,8 @@
+package jgo.compiler
+package transl
+package func
+
+import interm._
+import instr._
+
+case class UnsupportedInstrException(i: Instr) extends UnsupportedOperationException(i.toString)
