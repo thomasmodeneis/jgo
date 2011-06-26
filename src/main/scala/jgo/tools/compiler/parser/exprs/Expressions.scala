5cbf1822253cf764c01c9a14e98a8f107667b9e4
Harrison Klaperman <Harrison Klaperman@localhost>
2011-06-25 21:16:32 -0400

    Oops.  Forgot to save changes in Expressions.

--- src/main/scala/jgo/tools/compiler/parser/exprs/Expressions.scala
+++ src/main/scala/jgo/tools/compiler/parser/exprs/Expressions.scala
@@ -59,15 +59,10 @@ trait Expressions extends PrimaryExprs with ExprUtils {
     | "<-" ~ unaryExpr  ^^ chanRecv
     | "&"  ~ unaryExpr  ^^ addrOf
     | "*"  ~ unaryExpr  ^^ deref
-    | atomicExpr
-    )
-  
-  lazy val atomicExpr: Rule[Expr] = "atomic expr: primary or bfunc call" $
-    ( onlyBfuncSymbol ~ "(" ~ onlyGoType ~ expr0List <~ ")" ^^ typeInvoke
-    | onlyBfuncSymbol ~ "(" ~ expr0List <~ ")"              ^^ invoke
     | primaryExpr
     )
-    
+  
+  
   lazy val exprList: Rule[List[Expr]] =                "expression list" $
     rep1sep(expression, ",")
   
